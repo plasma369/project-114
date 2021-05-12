d94140a1f4fb6ea508fdf2c7671177a36d9d204e
plasma369 <76848965+plasma369@users.noreply.github.com>
2021-05-12 14:31:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function preload() {}
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+    mustache = loadImage('https://i.postimg.cc/SRYJnXxY/Mutache.jpg');
+}
 
 function setup()
 {
@@ -9,22 +15,12 @@ function setup()
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on("pose", gotPoses)
+    poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded()
 {
-    console.log("PoseNet is initialized!")
-}
-
-function draw()
-{
-    image(video, 0, 0, 300, 300);
-}
-
-function take_snapshot()
-{
-    save('myFilterImage.png');
+    console.log("PoseNet is initialized!");
 }
 
 function gotPoses(results)
@@ -32,7 +28,20 @@ function gotPoses(results)
     if(results.leangth > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y)
+        noseY = results[0].pose.nose.x-5;
+        noseX = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 94, 30);
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
 }
